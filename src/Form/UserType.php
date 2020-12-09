9d35e237f64c35998791701473d4ef700779ebcd
LaurentBouquet <laurent.bouquet@joliciel.fr>
2020-12-09 16:14:28 +0100

    [Core] Add user CRUD

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
@@ -13,7 +14,7 @@ class UserType extends AbstractType
     {
         $builder
             ->add('pseudo')
-            ->add('passwd')
+            ->add('passwd', PasswordType::class)
             ->add('date')
             ->add('about')
         ;
